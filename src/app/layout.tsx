6f9c57e9bffba826cf8aba84360fe95a90fd9ee1
Rangeet07 <rangeetnandy07@gmail.com>
2025-07-14 11:37:55 +0530

    schemats

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
 
-    <ClerkProvider>
+    <ConvexClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -33,6 +34,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-    </ClerkProvider>
+    </ConvexClerkProvider>
   );
 }
