213d9822bfaa0ee71bfdc4bb79777cffd3968a18
Rangeet07 <rangeetnandy07@gmail.com>
2025-07-15 22:23:49 +0530

    httpts1

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,5 @@
 import { httpRouter } from "convex/server";
-import { Webhook } from "lucide-react";
+import { Webhook } from "svix";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { api } from "./_generated/api"
 import {httpAction} from "./_generated/server"
@@ -27,12 +27,53 @@ http.route({
 
 
 
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+
+    try {
+      evt = wh.verify(body, {
+        "svix-id": svix_id,
+        "svix-timestamp": svix_timestamp,
+        "svix-signature": svix_signature,
+      }) as WebhookEvent;
+    } catch (err) {
+      console.error("Error verifying webhook:", err);
+      return new Response("Error occurred", { status: 400 });
+    }
+
+    const eventType = evt.type;
 
 
+    if (eventType === "user.created") {
+      const { id, first_name, last_name, image_url, email_addresses } = evt.data;
 
+      const email = email_addresses[0].email_address;
+
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.syncUser, {
+          email,
+          name,
+          image: image_url,
+          clerkId: id,
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", { status: 500 });
+      }
+    }
+
+
+    return new Response("Webhooks processed successfully", { status: 200});
 
 
     
 
     })
 })
+
+export default http;
